5328c83bd182ccb536927fc356f735be8ef1959c
Juan Miguel Caberta <caber_t@hotmail.com>
2022-03-16 18:38:41 +0100

    New class and DbContext

---
+++ src/Services/Catalog/Catalog.Persistance.Database/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Persistance.Database
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(
+            DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInStock> Stocks { get; set; }
+    }
+}
