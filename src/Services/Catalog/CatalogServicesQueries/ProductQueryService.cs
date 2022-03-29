53a16d767bcdb3936cd316be45071040b642375a
Juan Miguel Caberta <caber_t@hotmail.com>
2022-03-29 19:47:18 +0200

    new forlder common with 3 library class

---
+++ src/Services/Catalog/CatalogServicesQueries/ProductQueryService.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Persistance.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatalogServicesQueries
+{
+    public class ProductQueryService
+    {
+        private readonly AplicationDbContext _context;
+        public ProductQueryService(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public  async Task
+        {
+
+        }
+    }
+}
