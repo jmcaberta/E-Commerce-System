8cbd61506cccb785f322f6df9a596e3a7c1a04c3
Juan Miguel Caberta <caber_t@hotmail.com>
2022-03-30 19:21:30 +0200

    Varios

--- Service.Common.Paging/PagingExtension.cs
+++ Service.Common.Paging/PagingExtension.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Service.Common.Collection;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Service.Common.Paging
 {
-    internal class PagingExtension
+    public static class PagingExtension
     {
+        public static async Task<DataCollection<T>> GetPagedAsync<T>(
+            this IQueryable<T> query,
+            int page,
+            int take)
+        {
+            var originalPages = page;
+
+            page--;
+            if (page > 0)
+                page = page * take;
+
+            var result = new DataCollection<T>
+            {
+                Items = await query.Skip(page).Take(take).ToListAsync(),
+                Total = await query.CountAsync(),
+                Page = originalPages
+            };
+        }
     }
 }
